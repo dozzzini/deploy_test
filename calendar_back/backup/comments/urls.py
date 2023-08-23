a4b2d9a1d1ddf171c94e30a28c5bafd7e1952148
byungyong <quddyddlrk@naver.com>
2023-08-23 14:39:39 +0900

    backup 이동

--- calendar_back/backup/comments/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("newcomment/", views.NewComment.as_view()),
-    path("delete/<int:comment_id>/", views.DeleteComment.as_view()),
-]
--- calendar_back/backup/schedules/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-from .models import Schedule
-
-
-class ScheduleSerializer(ModelSerializer):
-    class Meta:
-        model = Schedule
-        fields = "__all__"
