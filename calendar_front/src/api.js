0bd5f75e02713f1732f2aadd0dc546f67ad0c89d
byungyong <quddyddlrk@naver.com>
2023-08-24 03:02:56 +0900

    api 주소 수정

--- calendar_front/src/api.js
+++ calendar_front/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://port-0-calendar-backend-ac2nll4pdsc1.sel3.cloudtype.app', //장고 서버 주소
+  baseURL: process.env.API_URL, //장고 서버 주소
   withCredentials: true, // 쿠키를 포함시키기 위한 설정 추가
 });
 
