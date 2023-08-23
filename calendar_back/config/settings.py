fbe168998d4b67cf52d3ab1abdaaa36192f51574
byungyong <quddyddlrk@naver.com>
2023-08-24 03:21:02 +0900

    migrations

--- calendar_back/config/settings.py
+++ calendar_back/config/settings.py
@@ -19,7 +19,11 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(",")
+try:
+    ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(",")
+except:
+    ALLOWED_HOSTS = ["*"]
+
 
 # Application definition
 CUSTOM_APPS = [
