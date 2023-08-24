2c0293c2dda392e84a7c0efb635a72290d1c47f0
byungyong <quddyddlrk@naver.com>
2023-08-24 17:34:23 +0900

    fixing csrf

--- calendar_front/src/api.js
+++ calendar_front/src/api.js
@@ -4,10 +4,10 @@ import Cookies from 'js-cookie';
 let csrftoken = Cookies.get('csrftoken');
 
 const instance = axios.create({
-  // headers: {
-  //   'Content-Type': 'application/json',
-  //   'X-CSRFToken': csrftoken,
-  // },
+  headers: {
+    'Content-Type': 'application/json',
+    'X-CSRFToken': csrftoken,
+  },
   baseURL: process.env.API_URL, //장고 서버 주소
   withCredentials: true, // 쿠키를 포함시키기 위한 설정 추가
 });
