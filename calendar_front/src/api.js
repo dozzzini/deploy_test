f62dd61dd75039ccb0a6d37f0a1b6e5e56cc1917
byungyong <quddyddlrk@naver.com>
2023-08-25 04:06:27 +0900

    로그인

--- calendar_front/src/api.js
+++ calendar_front/src/api.js
@@ -4,8 +4,8 @@ const instance = axios.create({
   headers: {
     'Content-Type': 'application/json',
   },
-  // baseURL: process.env.API_URL, //장고 서버 주소
-  baseURL: 'http://localhost:8000', //로컬
+  baseURL: process.env.API_URL, //장고 서버 주소
+  // baseURL: 'http://localhost:8000', //로컬
   withCredentials: true, // 쿠키를 포함시키기 위한 설정 추가
 });
 
