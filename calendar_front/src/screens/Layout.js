c9285de8bcc1ab9b6890170bdcca7d8144284a72
dozzzini <dozzzini@naver.com>
2023-08-25 16:35:45 +0900

    중복된 내용 삭제

--- calendar_front/src/screens/Layout.js
+++ calendar_front/src/screens/Layout.js
@@ -65,45 +65,4 @@ function Layout() {
   );
 }
 
-const Container = styled.div`
-  width: 100vw;
-  height: 100vh;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background-color: rgb(242, 242, 242);
-`;
-
-const Wrapper = styled.div`
-  display: flex;
-  max-width: 90vw;
-  height: 90vh;
-  background-color: rgb(254, 250, 250);
-  border-radius: 30px;
-  box-shadow:
-    12px 12px 13px #c8c4c4,
-    -12px -12px 13px #fff;
-`;
-
-function Layout() {
-  const [events, setEvents] = useRecoilState(eventState);
-  const [selectedEvent, setSelectedEvent] = useState(null);
-
-  return (
-    <RecoilRoot>
-      <Container>
-        <Wrapper>
-          <TUICalendar
-            events={events}
-            setEvents={setEvents}
-            view="month"
-            setSelectedEvent={setSelectedEvent}
-          />{' '}
-          <RightBar selectedEvent={selectedEvent} />
-        </Wrapper>
-      </Container>
-    </RecoilRoot>
-  );
-}
-
 export default Layout;
